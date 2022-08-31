a5396a00aaf62f5d0f7141c57af4113fddde3f20
Ilev Andrei <ilev.andrei@yandex.ru>
2022-09-01 00:29:48 +0300

    data_layer

--- app/src/main/java/com/bignerdranch/android/composition/domain/usecases/GenerateQuestionUseCase.kt
+++ app/src/main/java/com/bignerdranch/android/composition/domain/usecases/GenerateQuestionUseCase.kt
@@ -1,4 +1,18 @@
 package com.bignerdranch.android.composition.domain.usecases
 
-class GenerateQuestionUseCase {
+import com.bignerdranch.android.composition.domain.entity.Question
+import com.bignerdranch.android.composition.domain.repository.GameRepository
+
+class GenerateQuestionUseCase(
+    private val repository: GameRepository
+) {
+    //переопределение этого оператора можно использовать когда есть только метод
+    operator fun invoke(maxSumValue:Int): Question {
+       return repository.GenerateQuestion(maxSumValue, COUNT_OF_OPTIONS)
+    }
+
+    private companion object{
+
+        private const val COUNT_OF_OPTIONS=6
+    }
 }
--- app/src/main/java/com/bignerdranch/android/composition/domain/usecases/GetGameSettingsUseCase.kt
+++ app/src/main/java/com/bignerdranch/android/composition/domain/usecases/GetGameSettingsUseCase.kt
@@ -1,4 +1,14 @@
 package com.bignerdranch.android.composition.domain.usecases
 
-class GetGameSettingsUseCase {
+import com.bignerdranch.android.composition.domain.entity.GameSettings
+import com.bignerdranch.android.composition.domain.entity.Level
+import com.bignerdranch.android.composition.domain.repository.GameRepository
+
+class GetGameSettingsUseCase(
+    private val repository: GameRepository
+) {
+
+    operator fun invoke(level: Level): GameSettings {
+        return repository.getGameSettings(level)
+    }
 }
