df413142cf1688792dd6e80ce851e0d1fa18f75f
ilev <ilev.andrei@yandex.ru>
2022-09-08 15:42:24 +0300

    08.09 gameViewModel

--- app/src/main/java/com/bignerdranch/android/composition/presentation/GameFragment.kt
+++ app/src/main/java/com/bignerdranch/android/composition/presentation/GameFragment.kt
@@ -1,20 +1,47 @@
 package com.bignerdranch.android.composition.presentation
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.bignerdranch.android.composition.R
 import com.bignerdranch.android.composition.databinding.FragmentGameBinding
 import com.bignerdranch.android.composition.domain.entity.GameResult
-import com.bignerdranch.android.composition.domain.entity.GameSettings
 import com.bignerdranch.android.composition.domain.entity.Level
 
 
 class GameFragment : Fragment() {
 
-    private lateinit var level:Level
+
+    private lateinit var level: Level
+
+    //обратить внимание, ленивая инициализация
+    private val viewModel: GameViewModel by lazy {
+        ViewModelProvider(
+            this, ViewModelProvider
+                .AndroidViewModelFactory.getInstance(
+                    requireActivity()
+                        .application
+                )
+        )[GameViewModel::class.java]
+    }
+
+    private val tvOptions by lazy {
+        mutableListOf<TextView>().apply {
+            add(binding.tvOption1)
+            add(binding.tvOption2)
+            add(binding.tvOption3)
+            add(binding.tvOption4)
+            add(binding.tvOption5)
+            add(binding.tvOption6)
+
+        }
+    }
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
@@ -36,14 +63,57 @@ class GameFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //потом удалить
-        binding.tvQuestion.setOnClickListener{
-            launchGameFinishedFragment(
-                GameResult(
-                true,20,30,
-                GameSettings(0,0,0,0)
-            )
-            )
+        observeViewModel()
+        viewModel.startGame(level)
+    }
+
+    private fun setClickListenersToOptions(){
+
+    }
+
+    //подписываемся на объекты лайв дате
+    private fun observeViewModel() {
+        viewModel.question.observe(viewLifecycleOwner) {
+            binding.tvSum.text = it.sum.toString()
+            binding.tvLeftNumber.text = it.visibleNumder.toString()
+            for (i in 0 until tvOptions.size) {
+                tvOptions[i].text = it.options[i].toString()
+            }
         }
+
+        viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
+            binding.progressBar.setProgress(it, true)
+        }
+
+        viewModel.enoughCount.observe(viewLifecycleOwner) {
+            val color = getColorByState(it)
+            binding.tvAnswersProgress.setTextColor(getColorByState(it))
+        }
+        //установка цвета у прогресс бара
+        viewModel.enoughPercent.observe(viewLifecycleOwner) {
+            val color = getColorByState(it)
+            binding.progressBar.progressTintList= ColorStateList.valueOf(color)
+        }
+        viewModel.formattedTime.observe(viewLifecycleOwner){
+           binding.tvTimer.text=it
+        }
+        viewModel.minPercent.observe(viewLifecycleOwner){
+            binding.progressBar.secondaryProgress=it
+        }
+        viewModel.gameResult.observe(viewLifecycleOwner){
+            launchGameFinishedFragment(it)
+        }
+
+    }
+
+    private fun getColorByState(goodState: Boolean): Int {
+        val colorResId = if (goodState) {
+            android.R.color.holo_green_dark
+        } else {
+            android.R.color.holo_red_dark
+        }
+        return ContextCompat.getColor(requireContext(), colorResId)
+
     }
 
     /**
@@ -64,26 +134,26 @@ class GameFragment : Fragment() {
     /**
      * распаковка сериализуемого объекта
      */
-    private fun parseArgs(){
+    private fun parseArgs() {
         //явное приведение
-       requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
-            level=it
+        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+            level = it
         }
     }
 
-    companion object{
+    companion object {
 
-        const val NAME="GameFragment"
+        const val NAME = "GameFragment"
 
-        private const val KEY_LEVEL="level"
+        private const val KEY_LEVEL = "level"
 
         /**
          * фабричный метод для создания объектов GameFragment
          */
-        fun newInstance(level: Level):GameFragment{
+        fun newInstance(level: Level): GameFragment {
             return GameFragment().apply {
-                arguments=Bundle().apply{
-                    putParcelable(KEY_LEVEL,level)
+                arguments = Bundle().apply {
+                    putParcelable(KEY_LEVEL, level)
                 }
             }
         }
